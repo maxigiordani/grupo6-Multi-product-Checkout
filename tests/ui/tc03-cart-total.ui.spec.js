d707acf4b486537179ade2e0fcda7b510081261d
Stefan Dios Mayarin <stefandiosmayarin@gmail.com>
2026-06-08 06:26:49 -0300

    TC03: agregar dos productos al carrito

--- tests/ui/tc03-cart-total.ui.spec.js
+++ tests/ui/tc03-cart-total.ui.spec.js
@@ -1,5 +1,31 @@
-import { test, expect } from '@playwright/test';
+const { test, expect } = require('@playwright/test');
 
-test('TC03 - validar que el total es la suma correcta', async ({ page }) => {
-  await page.goto('https://www.demoblaze.com/');
+test('TC03 - agregar dos productos al carrito', async ({ page }) => {
+  await page.goto('/');
+
+  // Samsung Galaxy S6
+  await page.getByRole('link', { name: 'Samsung galaxy s6' }).click();
+
+  page.once('dialog', async (dialog) => {
+    await dialog.accept();
+  });
+
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+
+  await page.waitForTimeout(2000);
+
+  await page.getByRole('link', { name: 'Home' }).click();
+
+  // Sony Xperia Z5
+  await page.getByRole('link', { name: 'Sony xperia z5' }).click();
+
+  page.once('dialog', async (dialog) => {
+    await dialog.accept();
+  });
+
+  await page.getByRole('link', { name: 'Add to cart' }).click();
+
+  await page.waitForTimeout(2000);
+
+  await expect(page.getByRole('link', { name: 'Add to cart' })).toBeVisible();
 });
